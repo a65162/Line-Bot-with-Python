730316d122e4fb1497977f9ce9f1dff6e769081b
阿洧 <a65162@gmail.com>
2018-04-11 16:38:46 +0800

    add pattern_mega function

--- app.py
+++ app.py
@@ -38,6 +38,15 @@ def callback():
 
     return 'OK'
 
+def pattern_mega(text):
+    patterns = [
+        'mega', 'mg', 'mu', 'ＭＥＧＡ', 'ＭＥ', 'ＭＵ',
+        'ｍｅ', 'ｍｕ', 'ｍｅｇａ', 'GD', 'MG', 'google',
+    ]
+    for pattern in patterns:
+        if re.search(pattern, text, re.IGNORECASE):
+            return True
+
 def eyny_movie():
     target_url = 'http://www.eyny.com/forum-205-1.html'
     print('Start parsing eynyMovie....')
