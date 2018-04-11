13ac33d67ff23127edc1b7e85f8c0e218e962e43
阿洧 <a65162@gmail.com>
2018-04-11 16:26:13 +0800

    test eyny

--- app.py
+++ app.py
@@ -38,12 +38,31 @@ def callback():
 
     return 'OK'
 
-
+def eyny_movie():
+    target_url = 'http://www.eyny.com/forum-205-1.html'
+    print('Start parsing eynyMovie....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ''
+    for titleURL in soup.select('.bm_c tbody .xst'):
+        if pattern_mega(titleURL.text):
+            title = titleURL.text
+            if '11379780-1-3' in titleURL['href']:
+                continue
+            link = 'http://www.eyny.com/' + titleURL['href']
+            data = '{}\n{}\n\n'.format(title, link)
+            content += data
+    return content
+    
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+if event.message.text == "eyny":
+        content = eyny_movie()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
 
 
 if __name__ == "__main__":
